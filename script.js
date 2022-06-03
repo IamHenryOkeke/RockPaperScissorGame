95ab014fee26a7eca296f3994e0083cf5d295c0e
Henry Okeke <iamhenryokeke@gmail.com>
2022-06-03 11:07:57 +0100

    Included the function for gameplay with five rounds of play

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // this is a script for rock paper scissors game
-// the function generates a number between 1 - 3 and returns either rock, paper or scissors value depending on the value
 
+// the function generates a number between 1 - 3 and returns either rock, paper or scissors value depending on the value
 function computerPlay(){
     let number = Math.floor((Math.random() * 3) + 1);
     // generates an integer between 1 and 3
@@ -21,35 +21,69 @@ function computerPlay(){
 // this function compares the player input and the computerChoice to decide who won
 function playRound(computerSelection, playerSelection){
     let message;
-    if (computerSelection === (playerSelection.toLowerCase())){
-    message =  console.log(`It's a draw. You both chose ${playerSelection}`)
+    if (computerSelection === playerSelection){
+        message = `It's a draw. You both chose ${playerSelection}`;
+        
     }
-    else if (computerSelection === "rock" && (playerSelection.toLowerCase()) === "paper"){
-        message =  console.log(`The computer chose ${computerSelection} \nYou win. ${playerSelection} covers ${computerSelection}.`)
+    else if (computerSelection === "rock" && playerSelection === "paper"){
+        message = `The computer chose ${computerSelection} \nYou win. ${playerSelection} covers ${computerSelection}.`;
+        
     }
-    else if (computerSelection === "rock" && (playerSelection.toLowerCase()) === "scissors"){
-        message =  console.log(`The computer chose ${computerSelection} \nYou lose. ${computerSelection} crushes ${playerSelection}.`)
+    else if (computerSelection === "rock" && playerSelection === "scissors"){
+        message = `The computer chose ${computerSelection} \nYou lose. ${computerSelection} crushes ${playerSelection}.`;   
     }
-    else if (computerSelection === "paper" && (playerSelection.toLowerCase()) === "rock"){
-        message =  console.log(`The computer chose ${computerSelection} \nYou lose. ${computerSelection} covers ${playerSelection}.`)
+    else if (computerSelection === "paper" && playerSelection === "rock"){
+        message = `The computer chose ${computerSelection} \nYou lose. ${computerSelection} covers ${playerSelection}.`;
+        
     }
-    else if (computerSelection === "paper" && (playerSelection.toLowerCase()) === "scissors"){
-        message =  console.log(`The computer chose ${computerSelection} \nYou win. ${playerSelection} cuts ${computerSelection}.`)
+    else if (computerSelection === "paper" && playerSelection === "scissors"){
+        message = `The computer chose ${computerSelection} \nYou win. ${playerSelection} cuts ${computerSelection}.`;
+        
     }
-    else if (computerSelection === "scissors" && (playerSelection.toLowerCase()) === "paper"){
-        message =  console.log(`The computer chose ${computerSelection} \nYou lose. ${computerSelection} cuts ${playerSelection}.`)
+    else if (computerSelection === "scissors" && playerSelection === "paper"){
+        message = `The computer chose ${computerSelection} \nYou lose. ${computerSelection} cuts ${playerSelection}.`;
+        
     }
-    else if (computerSelection === "scissors" && (playerSelection.toLowerCase()) === "rock"){
-        message =  console.log(`The computer chose ${computerSelection} \nYou win. ${playerSelection} crushes ${computerSelection}.`)
+    else if (computerSelection === "scissors" && playerSelection === "rock"){
+        message = `The computer chose ${computerSelection} \nYou win. ${playerSelection} crushes ${computerSelection}.`;
+        
     }
     else{
-        console.log("Wrong entry. Enter from the options provided")
+        console.log("Wrong entry. Enter from the options provided");
     }
     return message;
 }
 
-const computerSelection = computerPlay();
-// console.log(computerSelection)
-let playerSelection = (prompt("Enter either rock,paper or scissors: ", "rock"));
-// console.log(playerSelection);
-console.log(playRound(computerSelection,playerSelection))
+//this function runs the game taking in playRound and ComputerPlay function
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+    let winnerMessage;
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection = (prompt("Enter either rock,paper or scissors: ", "rock")).toLowerCase();
+        let statement = playRound(computerSelection, playerSelection);
+        if (statement.includes("win")){
+            playerScore++;
+            
+        }
+        else if (statement.includes("lose")){
+            computerScore++;
+        }
+        else{
+            console.log("Draw")
+        }
+        console.log(`${playerScore} - ${computerScore}`);
+     }
+     // displays the winner depending on the score
+     if (playerScore > computerScore){
+         winnerMessage = "After 5 rounds, You win";
+     }
+     else{
+         winnerMessage = "After 5 rounds, You lose"
+     }
+     return console.log(winnerMessage);
+}
+
+// runs the game from the console
+console.log(game())
