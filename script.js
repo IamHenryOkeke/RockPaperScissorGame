1cfcec7993b75b9f19f62f5094721969daa2e4f8
Henry Okeke <iamhenryokeke@gmail.com>
2022-06-21 03:32:35 +0100

    Refactored code and functionality

--- script.js
+++ script.js
@@ -1,154 +1,90 @@
+const mainContainer = document.querySelector(".main-container")
+
 const container = document.querySelector(".container")
 
 const body = document.querySelector("body")
 
 const head = document.createElement('h1');
 
-const para = document.createElement("p");
+const para1 = document.createElement("p");
 
-const btnRock = document.createElement("button");
+const para2 = document.createElement("p");
 
-const btnScissors = document.createElement("button");
+const result = document.getElementById("result");
 
-const btnPaper = document.createElement("button");
+const score = document.getElementById("score");
 
-const content = document.querySelector('#result');
+const buttons = document.querySelectorAll('button');
 
-const result = document.createElement("div")
-
-const resultPlay = document.createElement("div")
-
-
-
+body.setAttribute("style", "background-color: grey;max-width: 500px;margin: auto; color:white;");
 
 head.classList.add('head');
 head.textContent = "Rock Paper Scissors Game"
 container.appendChild(head);
 
-para.classList.add('para');
-para.textContent = 'Pick your selection below via the button to play against the computer.'
-container.appendChild(para);
+para1.classList.add('para1');
+para1.textContent = 'Pick your selection below via the button to play against the computer.'
+container.appendChild(para1);
 
-btnRock.classList.add('buttonRock');
-btnRock.textContent = "Rock"
-btnRock.setAttribute('style', 'margin-right: 10px; padding: 8px 15px;background-color: red; border-radius: 10px;');
-btnRock.addEventListener('click', () => {
-    let computerSelection = computerPlay();
-    let playerSelection = "rock";
-    result.classList.add('result');
-    result.textContent = playRound(computerSelection, playerSelection);
-    container.appendChild(result);
-  });
-container.appendChild(btnRock);
+para2.classList.add('para2');
+para2.innerHTML = 'Dear Nigerian Student, Get to five first to beat the computer. The fate of ASUU strike is in your hands. Just kidding &#129315	'
+container.appendChild(para2);
 
-btnScissors.classList.add('buttonScissors');
-btnScissors.textContent = "Scissors"
-btnScissors.setAttribute('style', 'margin-right: 10px; padding: 8px 15px;background-color: yellow; border-radius: 10px;');
-btnScissors.addEventListener('click', () => {
-    let computerSelection = computerPlay();
-    let playerSelection = "scissors";
-    result.classList.add('result');
-    result.textContent = playRound(computerSelection, playerSelection);
-    container.appendChild(result);
-  });
-container.appendChild(btnScissors);
 
-btnPaper.classList.add('buttonPaper');
-btnPaper.textContent = "Paper"
-btnPaper.setAttribute('style', 'margin-right: 10px; margin-bottom: 50px; padding: 8px 15px;background-color: green;border-radius: 10px;');
-btnPaper.addEventListener('click', () => {
-    let computerSelection = computerPlay();
-    let playerSelection = "paper";
-    result.classList.add('result');
-    result.textContent = playRound(computerSelection, playerSelection);
-    container.appendChild(result);
-  });
-container.appendChild(btnPaper);
-
-body.setAttribute("style", "background-color: grey;max-width: 500px;margin: auto;");
-
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
 function computerPlay(){
     let myArray = ["rock", "paper", "scissors"];
-    let number = Math.floor((Math.random() * (myArray.length)) + 1);
+    let number = Math.floor((Math.random() * myArray.length));
     // generates an integer between 1 and 3
-    let computerChoice;
-    if (number === 1){
-        computerChoice = myArray[0];
-    }
-    else if (number === 2){
-        computerChoice = myArray[1];
-    }
-    else{
-        computerChoice = myArray[2];
-    }
-    // set computerChoice to either rock, paper or scissors value
-    return computerChoice;
+    return myArray[number];
+}
+function stopPlay() {
+    buttons.forEach(btn => {
+        btn.disabled = true
+    })
 }
 
-// this function compares the player input and the computerChoice to decide who won
-function playRound(computerSelection, playerSelection){
+function playRound(playerSelection){
+    let computerSelection = computerPlay()
     let message;
     if (computerSelection === playerSelection){
-        message = `It's a draw. You both chose ${playerSelection}`;
-        
+        message = `It's a draw. You both chose ${playerSelection}`; 
+        result.textContent = message;
+        mainContainer.appendChild(result);
+        score.textContent = (`Score: Player ${playerScore} - ${computerScore} Computer`) 
     }
     else if (computerSelection === "rock" && playerSelection === "paper" || 
         computerSelection === "paper" && playerSelection === "scissors" || 
         computerSelection === "scissors" && playerSelection === "rock"){
-        message = `The computer chose ${computerSelection} \nYou win.`;
-        
-    }
-    else if (computerSelection === "rock" && playerSelection === "scissors" || 
-        computerSelection === "paper" && playerSelection === "rock" ||
-        computerSelection === "scissors" && playerSelection === "paper"){
-        message = `The computer chose ${computerSelection} \nYou lose.`;   
-    }
-    return message;
-}
-
-// this function runs the game taking in playRound and ComputerPlay function
-function gamePlay(){
-    let computerScore = 0;
-    let playerScore = 0;
-    let winnerMessage;
-    let i = 0;
-    while (playerScore !== 5 || computerScore !== 5) {
-        let computerSelection = computerPlay();
-        let playerSelection = (prompt("Enter either rock,paper or scissors: ", "rock")).toLowerCase();
-        let statement = playRound(computerSelection, playerSelection);
-        if (playerScore === 5 || computerScore === 5){
-            break;
+        message = `The computer chose ${computerSelection} \nYou win.`; 
+        playerScore++; 
+        result.textContent = message;
+        mainContainer.appendChild(result);
+        score.textContent = (`Score: Player ${playerScore} - ${computerScore} Computer`);
+        if (playerScore === 5) {
+            result.innerHTML = 'You got to five first.You won the game! &#129327. Reload the page to play again'
+            stopPlay();
         } 
-        
-        else{
-            if (statement.includes("win")){
-                playerScore++;
-                
-            }
-            else if (statement.includes("lose")){
-                computerScore++;
-            }
-            else{
-                console.log("Draw")
-            }
-            console.log(`${playerScore} - ${computerScore}`);
-        }
-        i++;
-     }
-     // displays the winner depending on the score
-     if (playerScore > computerScore){
-         winnerMessage = "You win";
-     }
-     else if (playerScore == computerScore){
-         winnerMessage = "It was a draw"
-     }
-     else{
-         winnerMessage = "You lose"
-     }
-     return console.log(winnerMessage);
+    }
+    else{
+        message = `The computer chose ${computerSelection} \nYou lose.`; 
+        computerScore++;
+        result.textContent = message;
+        mainContainer.appendChild(result);
+        score.textContent = (`Score: Player ${playerScore} - ${computerScore} Computer`);
+        if (computerScore === 5) {
+            result.innerHTML = 'The Computer got to five first.You lose! &#128541. Reload the page to play again'
+            stopPlay();
+        } 
+    }
 }
 
-// runs the game from the console
-// let computerSelection = computerPlay();
-// // let playerSelection = (prompt("Enter either rock,paper or scissors: ", "rock")).toLowerCase();
-// console.log(playRound(computerSelection, playerSelection));
+buttons.forEach(button => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        playerSelection = button.value;
+        playRound(playerSelection);
+    });
+});
